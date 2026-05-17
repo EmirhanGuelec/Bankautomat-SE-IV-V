487874d7d8efb0ae9099d9901b30b15e1966b763
TomHoreldt <horeldt_tom@teams.hs-ludiwgsburg.de>
2026-05-17 14:22:36 +0200

    Backend Login, Abheben nun sinngemäß

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("transaktionen/", views.transaktionen, name="transaktionen"),
-    path("kontostand/", views.kontostand, name="kontostand"),
-    path("abheben/", views.abheben, name="abheben"),
-    path("pin/", views.pin, name="pin"),
-    path("ausgeloggt/", views.ausgeloggt, name="ausgeloggt"),
+    path('', views.home, name='home'),
+    path('api/rfid-login/', views.rfid_login, name='rfid_login'),
+    path('api/abheben/', views.abheben_api, name='abheben_api'),
+    path('logout/', views.logout_view, name='logout'),
+    path('transaktionen/', views.transaktionen, name='transaktionen'),
+    path('kontostand/', views.kontostand, name='kontostand'),
+    path('abheben/', views.abheben, name='abheben'),
+    path('pin/', views.pin, name='pin'),
+    path('ausgeloggt/', views.ausgeloggt, name='ausgeloggt'),
 ]
